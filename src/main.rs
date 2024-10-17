b7c2cc307e1b9635b9dd9ec57806d5272cf87d58
Ogi-Git <ognjen25aleksic@gmail.com>
2024-10-17 15:21:43 +0200

    Move from Actix to Axum

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,21 @@
-use std::sync::Mutex;
-mod app_config;
+#![allow(unused)]
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use app_config::{app_config, scoped_config};
+use std::os::unix::net::SocketAddr;
 
-struct AppState {
-    counter: Mutex<i32>
+use axum::{Router, routing::get, response::Html};
+
+async fn hello() -> &'static str {
+    "Hello Solana World"
 }
 
-#[get("/users/{user_id}/{friend}")]
-async fn get_user(path: web::Path<(u32, String)>) -> impl Responder {
-    let (user_id, friend) = path.into_inner();
-    HttpResponse::Ok().body(format!("Welcome {}, user_id {}!", friend, user_id))
- }
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route(
+        "/hello", get(hello));
+        
+    let addr = "0.0.0.0:3000";
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
-async fn index(data: web::Data<AppState>) -> String {
-    let mut counter = data.counter.lock().unwrap();
-    *counter += 1;
-    format!("Request number: {counter}")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-
-    let counter = web::Data::new(AppState { counter: Mutex::new(0) });
-
-    HttpServer::new(move || {
-        App::new().app_data(counter.clone())
-        .configure(app_config)
-        .service(web::scope("/api").configure(scoped_config))
-        .service(get_user)
-        .route("/", web::get().to(index))
-    }).bind(("127.0.0.1", 3000))?.run().await
+    axum::serve(listener, app).await.unwrap();
 }
