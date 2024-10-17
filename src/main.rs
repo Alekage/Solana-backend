6a9779d190468406963ce6a4f592555e28acbe9d
Ogi-Git <ognjen25aleksic@gmail.com>
2024-10-17 16:52:18 +0200

    Add path to function argument

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 #![allow(unused)]
 
 use std::net::SocketAddr;
-
-use axum::{Router, routing::get, response::Html};
+use axum::{extract::{Query,Path }, response::{Html, IntoResponse}, routing::get, Router};
+use serde::Deserialize;
 use log::info;
 
-async fn hello_handler() -> &'static str {
-    "Hello Solana World"
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    chain_name: Option<String>
+}
+
+async fn hello_handler(Path(name): Path<String>) -> impl IntoResponse {
+    Html(format!("Hello, {}", name))
 }
 
 #[tokio::main]
@@ -14,11 +19,13 @@ async fn main() {
     env_logger::init();
 
     let app = Router::new().route(
-        "/hello", get(hello_handler));
+        "/hello/:name", get(hello_handler));
         
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
     info!("Listening on address: {:?}", addr);
-
+        // alternative:
+        // let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+        // axum::serve(listener, app).await.unwrap();
     axum_server::bind(addr).serve(app.into_make_service()).await.unwrap();
 }
